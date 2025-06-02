8785ff0e6f05cf15dd94a6ee8e4ef8575eaf8ede
Artur Khelshtein <artur.khelshtein@live.com>
2025-06-02 22:52:14 +0300

    feat: swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as path from 'path';
+import * as YAML from 'yamljs';
+import * as swaggerUi from 'swagger-ui-express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  const apiYamlPath = path.join(__dirname, '..', 'doc', 'api.yaml');
+  const swaggerDocument = YAML.load(apiYamlPath);
+
+  app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
   await app.listen(process.env.PORT || 4000);
 }
 
